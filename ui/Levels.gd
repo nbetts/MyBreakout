cf26b938e74e9f2e12086c37fc9328dbcfcb942c
Nathan Betts <nathanbetts@hotmail.co.uk>
2020-08-04 00:08:06 +0100

    make UI look a little nicer

--- ui/Levels.gd
+++ ui/Levels.gd
@@ -10,17 +10,24 @@ func _ready():
 	dir.open("res://levels")
 	dir.list_dir_begin()
 	var file = dir.get_next()
+	var levels = []
 	
 	while file != "":
 		var regex_result = level_file_regex.search(file.get_basename())
 		if regex_result:
 			var level = int(regex_result.get_string("level"))
-			var level_button = Button.new()
-			level_button.text = "%02d" % level
-			level_button.connect("pressed", self, "level_button_press", [level])
-			add_child(level_button)
-		
+			levels.append(level)
+			
 		file = dir.get_next()
+	
+	levels.sort()
+
+	for level in levels:
+		var level_button = MyButton.new()
+		level_button.text = "%02d" % level
+		print('level ', level)
+		level_button.connect("pressed", self, "level_button_press", [level])
+		add_child(level_button)
 
 
 func level_button_press(level):
